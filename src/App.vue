a76221994fc77e85af99faa76b46a0f3753a015b
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2021-11-05 11:44:53 +0000

    fix: align the content in the center

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="min-h-screen bg-neutral-200 flex place-content-center lg:py-11">
+  <div
+    class="
+      min-h-screen
+      bg-neutral-200
+      flex
+      items-center
+      justify-center
+      lg:py-11
+    "
+  >
     <div class="mt-12 w-full max-w-xl lg:mt-0 lg:max-w-5xl">
       <MyHeader />
       <MyTip />
