47cd04cc57dec5040c8f319ae042605746c3dfd8
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2021-11-08 10:50:14 +0000

    perf: use self host fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,26 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Space Mono', ...fontFamily.sans]
+        sans: ["Space Mono"],
       },
       colors: {
-        primary: 'hsl(172, 67%, 45%)',
+        primary: "hsl(172, 67%, 45%)",
         neutral: {
-          '100': 'hsl(189, 41%, 97%)',
-          '200': 'hsl(185, 41%, 84%)',
-          '300': 'hsl(184, 14%, 56%)',
-          '400': 'hsl(186, 14%, 43%)',
-          '500': 'hsl(183, 100%, 15%)',
+          100: "hsl(189, 41%, 97%)",
+          200: "hsl(185, 41%, 84%)",
+          300: "hsl(184, 14%, 56%)",
+          400: "hsl(186, 14%, 43%)",
+          500: "hsl(183, 100%, 15%)",
         },
-      }
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
